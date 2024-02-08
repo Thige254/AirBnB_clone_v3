4fc2c842644283fae1c9865988e86b59e021c553
Thige254 <thigetj@gmail.com>
2024-02-08 19:17:26 +0000

    updated index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,16 @@
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """Returns the API status as a JSON object."""
-    return jsonify({'status': 'OK'})
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Returns the number of objects of each type as a JSON object."""
+    stats = {
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User),
+    }
+    return jsonify(stats)
